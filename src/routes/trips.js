3602319d7a5157a824659e5ff5988296097123c0
Mufli Fadla Syihab <muflifadlayt38@gmail.com>
2024-11-30 07:07:42 +0800

    chore: add testing route

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const tripController = require("../controllers/trips");
 
-router.get('/trips', tripController.getTrips);
-router.get('/demand-trend', tripController.getDemandTrend);
-router.get('/income-trend', tripController.getIncomeTrend);
+router.get("/", (req, res) => {
+  res.send("API is working!");
+});
+
+router.get("/trips", tripController.getTrips);
+router.get("/demand-trend", tripController.getDemandTrend);
+router.get("/income-trend", tripController.getIncomeTrend);
 
 module.exports = router;
